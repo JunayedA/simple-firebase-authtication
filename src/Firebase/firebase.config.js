b42af967996c04589ebf51cf7a6b752a7066fc8c
JunayedAhmed <junayedahmed11099@gmail.com>
2021-10-07 21:14:12 +0600

    user in console

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAnp8MCg83CMtAV-tBnuu2xOPYRD3onY34",
+    authDomain: "simple-firebase-authenti-db769.firebaseapp.com",
+    projectId: "simple-firebase-authenti-db769",
+    storageBucket: "simple-firebase-authenti-db769.appspot.com",
+    messagingSenderId: "988262427178",
+    appId: "1:988262427178:web:62c98e2ba71c7bcd527153"
+  };
+
+  export default firebaseConfig;
